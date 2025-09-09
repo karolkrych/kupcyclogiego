1d7efd5d307e52c1513176b5d31468e58a967d4a
karlkrych <karol.krych77@gmail.com>
2025-09-09 16:10:45 +0200

    fourth attempt

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/kupcyclogiego/",   // ← NAZWA TWOJEGO REPO!
+  base: "/kupcyclogiego/",    // nazwa repo na GitHub Pages
   plugins: [react()],
 });
